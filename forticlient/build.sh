75e7c0d35a20802f547b5a7797e0756230867d4a
Marco Antônio Queiroz <marco.aq7@gmail.com>
2021-11-26 21:34:22 -0300

    Create proxy

--- forticlient/build.sh
+++ forticlient/build.sh
@@ -2,3 +2,4 @@
 
 docker build . --no-cache -t vpn
 docker run -it --name vpn-test1 --privileged -e "VPNADDR=vpn.ssa-br.com:10443" -e "VPNUSER=marco.queiroz" -e "VPNPASS=PASS" -p 22222:22 vpn:latest
+ssh -C -D 10080 -N localhost -p 22222 
